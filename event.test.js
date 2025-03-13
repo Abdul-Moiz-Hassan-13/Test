906153ca7973fa81865bbe4336a4c6847e490de8
Abdul-Moiz-Hassan-13 <abdulmoizhassan13@gmail.com>
2025-03-13 09:55:47 +0500

    Fifth commit

--- event.test.js
+++ event.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const mongoose = require('mongoose');
-const app = require('../server');
+const app = require('../app');
 const User = require('../User');
 const Event = require('../Event');
 
